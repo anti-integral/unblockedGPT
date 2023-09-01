d1e88f78c55ff022e5a7e50524e7dd1fd36da158
osanan25 <68397796+osanan25@users.noreply.github.com>
2023-09-01 04:58:40 -0600

    Update app.py

--- unblockedGPT/app.py
+++ unblockedGPT/app.py
@@ -1,8 +1,42 @@
 import streamlit as st
+import openai
 
+# Initialize conversation state
+conversation = []
+
+# Title
 st.title('UnblockedGPT Chatbot')
 
+# Model selection
+model_selection = st.selectbox('Select the model:', ['gpt-3.5-turbo', 'gpt-4'])
+
+# User input
 user_input = st.text_input('You: ')
 
+# Add user input to conversation
 if user_input:
-    st.write(f'Chatbot: Hello, you said {user_input}')
+    conversation.append({"role": "user", "content": user_input})
+
+# Display conversation history
+st.write("### Conversation:")
+for turn in conversation:
+    if turn["role"] == "user":
+        st.write(f"You: {turn['content']}")
+    elif turn["role"] == "assistant":
+        st.write(f"UnblockedGPT ({model_selection}): {turn['content']}")
+
+# Chatbot logic
+if user_input:
+    # Make API call to OpenAI GPT model (replace 'your_openai_api_key_here' with your actual API key)
+    openai.api_key = "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"
+    model_engine = model_selection
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=conversation,
+        max_tokens=100
+    )
+
+    # Extract and display chatbot's reply
+    chatbot_response = response.choices[0].text.strip()
+    conversation.append({"role": "assistant", "content": chatbot_response})
+    st.write(f"UnblockedGPT ({model_selection}): {chatbot_response}")
