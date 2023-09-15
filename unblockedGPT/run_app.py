b850aabf53c39a4570e1ed353158b90729223684
osanan25 <68397796+osanan25@users.noreply.github.com>
2023-09-14 21:39:42 -0600

    Update run_app.py

--- unblockedGPT/run_app.py
+++ unblockedGPT/run_app.py
@@ -4,4 +4,4 @@ import streamlit as st
 def run():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     app_path = os.path.join(dir_path, 'app.py')
-    st.command(f"streamlit run {app_path}")
+    os.system(f"streamlit run {app_path}")
