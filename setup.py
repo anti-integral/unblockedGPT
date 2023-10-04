3314597568ffde74030126be55a8b567b4c6f9af
osanan25 <om.sanan007@gmail.com>
2023-10-04 11:25:28 -0400

    Completely replacing repository content

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unblockedGPT',
-    version='0.3.11',
+    version='0.7.1',
     packages=find_packages(),
     install_requires=[
         'streamlit',
--- unblockedGPT/run_app.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def run():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    app_path = os.path.join(dir_path, 'app.py')
-    os.system(f'streamlit run {app_path}')
